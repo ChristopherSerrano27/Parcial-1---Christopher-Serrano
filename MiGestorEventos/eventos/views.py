e5af5dceee1aaa74115040d2d62e93bd28cfa5d2
Christopher Serrano <tyson.272002@gmail.com>
2024-09-05 11:50:04 -0500

    se añadio la logica a views para llenar el form de eventos

--- MiGestorEventos/eventos/views.py
+++ MiGestorEventos/eventos/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Evento
+from .forms import EventoForm
 
 def eventos_lista(request):
     eventos = Evento.objects.select_related('organizador').all()
     return render(request, 'eventos_lista.html', {'eventos': eventos})
+
+
+def crear_evento(request):
+    if request.method == 'POST':
+        form = EventoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('eventos_lista')  # Redirige a la lista de eventos después de guardar
+    else:
+        form = EventoForm()
+    
+    return render(request, 'crear_evento.html', {'form': form})
