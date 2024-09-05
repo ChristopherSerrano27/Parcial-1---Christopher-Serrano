a42fbc34f39a47d345e82235c88a161773ab35a9
Christopher Serrano <tyson.272002@gmail.com>
2024-09-05 11:03:50 -0500

    creacion de modelos "organizador" y "eventos"

--- MiGestorEventos/eventos/models.py
+++ MiGestorEventos/eventos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Organizador(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Evento(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    fecha = models.DateTimeField()
+    
+    #relacion
+    organizador = models.ForeignKey(Organizador, on_delete=models.CASCADE, related_name='eventos')
+
+    def __str__(self):
+        return self.nombre
