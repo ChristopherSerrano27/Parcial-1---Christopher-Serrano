2bfe1c76930b5a0d4430ba1f0d428494581a5f37
Christopher Serrano <tyson.272002@gmail.com>
2024-09-06 17:54:57 -0500

    se añadieron las rutas de organizadores y se ajustaron rutas de eventos

--- MiGestorEventos/eventos/urls.py
+++ MiGestorEventos/eventos/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.eventos_lista, name='eventos_lista'),
-    path('crear/', views.crear_evento, name='crear_evento'),
+    path('eventos/', views.eventos_lista, name='eventos_lista'),
+    path('eventos/crear/', views.crear_evento, name='crear_evento'),
+
+#2.4, organizadores
+
+    path('organizadores/', views.OrganizadoresListaView.as_view(), name= 'organizadores_lista'),
+    path('organizadores/crear/', views.OrganizadorCrearView.as_view(), name= 'crear_organizador'),
 ]
