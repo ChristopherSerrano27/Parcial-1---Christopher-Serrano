87f0ed95ca57ef70188726b679dd0c27793f2bb4
Christopher Serrano <tyson.272002@gmail.com>
2024-09-06 17:55:22 -0500

    se añadieron las vistas de organizadores

--- MiGestorEventos/eventos/views.py
+++ MiGestorEventos/eventos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Evento
-from .forms import EventoForm
+from .forms import EventoForm, OrganizadorForm
+
+from django.views.generic import ListView, CreateView
+from .models import Organizador
 
 def eventos_lista(request):
     eventos = Evento.objects.select_related('organizador').all()
@@ -12,8 +15,21 @@ def crear_evento(request):
         form = EventoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('eventos_lista')  # Redirige a la lista de eventos después de guardar
+            return redirect('eventos_lista')  
     else:
         form = EventoForm()
     
     return render(request, 'crear_evento.html', {'form': form})
+
+#PUNTO 2.4
+
+class OrganizadoresListaView(ListView):
+    model = Organizador
+    template_name = 'organizadores_lista.html'
+    context_object_name = 'organizadores'
+
+class OrganizadorCrearView(CreateView):
+    model = Organizador
+    form_class = OrganizadorForm
+    template_name = 'crear_organizador.html'
+    success_url = '/organizadores/'
